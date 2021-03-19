0bd2e9f55aa80af71d8870c06c63f17e8af75f22
Liz Oakes <liz77oakes@gmail.com>
2021-03-19 11:43:12 -0400

    edit task descriptions

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -45,7 +45,52 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+  
+  // replace p element with a new textarea
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  $(this).replaceWith(textInput);
+  // auto focus new element
+  textInput.trigger("focus");
+});
 
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function(){
+  //get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
+  //get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+   
+  // update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveTasks();
+  
+  //recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+
+});
 
 
 // modal was triggered
@@ -93,5 +138,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
